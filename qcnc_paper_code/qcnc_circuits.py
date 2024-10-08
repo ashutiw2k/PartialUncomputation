76b36a8c87e16ea808f0ce56d05034e892d1b9d4
Ashutosh Tiwari <ashutiw2k@gmail.com>
2024-10-08 04:50:54 -0400

    Figures and code for uncomputing a single ancilla (a0)

--- qcnc_paper_code/qcnc_circuits.py
+++ qcnc_paper_code/qcnc_circuits.py
@@ -86,6 +86,46 @@ def basic_circuit_with_uncomp(dir='qcnc_paper_code/'):
 
     return circuit, dir+'qcnc_paper_figures/basic_circuit_with_uncomp'
 
+def basic_circuit_with_sub_uncomp(dir='qcnc_paper_code/'):
+    input_q = QuantumRegister(2, 'q')
+    input_c = ClassicalRegister(2, 'qc')
+    ancilla_q = QuantumRegister(2, 'a')
+    ancilla_c = ClassicalRegister(2, 'ac')
+
+    circuit = QuantumCircuit(input_q, input_c, ancilla_q, ancilla_c)
+    circuit.h(input_q[0])
+    circuit.h(input_q[1])
+
+    # This is to group the gates in the drawing better
+    circuit.barrier(range(4))
+    
+    circuit.cx(input_q[0], ancilla_q[0])
+    circuit.cx(input_q[1], ancilla_q[1])
+
+
+    circuit.barrier(range(4))
+    # circuit.cx(ancilla_q[0], input_q[0])
+
+
+    circuit.cx(input_q[0], ancilla_q[0])
+    # circuit.cx(input_q[1], ancilla_q[1])
+
+    circuit.barrier(range(4))
+
+    circuit.h(input_q[0])
+    circuit.h(input_q[1])
+
+    circuit.barrier(range(4))
+
+    circuit.measure(qubit=input_q[0], cbit=input_c[0])
+    circuit.measure(qubit=input_q[1], cbit=input_c[1])
+    circuit.measure(qubit=ancilla_q[0], cbit=ancilla_c[0])
+    circuit.measure(qubit=ancilla_q[1], cbit=ancilla_c[1])
+    circuit.draw('mpl', initial_state=True, plot_barriers=False, filename=dir+'qcnc_paper_figures/basic_circuit_with_sub_uncomp')
+
+    return circuit, dir+'qcnc_paper_figures/basic_circuit_with_sub_uncomp'
+
 # print(os.)
 run_measurements(basic_circuit())
 run_measurements(basic_circuit_with_uncomp())
+run_measurements(basic_circuit_with_sub_uncomp())
