2e75652dd40aa3d63ec095d682408e0c5f0f0545
Ashutosh Tiwari <ashutiw2k@gmail.com>
2024-10-30 18:38:36 -0400

    Code and pics

--- qcnc_paper_code/qcnc_circuits.py
+++ qcnc_paper_code/qcnc_circuits.py
@@ -45,7 +45,7 @@ def basic_circuit(dir='qcnc_paper_code/'):
 
     circuit.barrier(range(4))
 
-    circuit.draw('mpl', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/basic_circuit')
+    circuit.draw('latex', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/basic_circuit.png')
 
     circuit.measure(qubit=input_q[0], cbit=input_c[0])
     circuit.measure(qubit=input_q[1], cbit=input_c[1])
@@ -85,7 +85,7 @@ def basic_circuit_with_uncomp(dir='qcnc_paper_code/'):
 
     circuit.barrier(range(4))
 
-    circuit.draw('mpl', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/basic_circuit_with_uncomp')
+    circuit.draw('latex', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/basic_circuit_with_uncomp.png')
 
     circuit.measure(qubit=input_q[0], cbit=input_c[0])
     circuit.measure(qubit=input_q[1], cbit=input_c[1])
@@ -125,7 +125,7 @@ def basic_circuit_with_sub_uncomp(dir='qcnc_paper_code/'):
 
     circuit.barrier(range(4))
     
-    circuit.draw('mpl', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/basic_circuit_with_sub_uncomp')
+    circuit.draw('latex', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/basic_circuit_with_sub_uncomp.png')
 
     circuit.measure(qubit=input_q[0], cbit=input_c[0])
     circuit.measure(qubit=input_q[1], cbit=input_c[1])
@@ -143,7 +143,7 @@ def multi_cnot_simple(dir='qcnc_paper_code/'):
     circuit = QuantumCircuit(c0,c1,c2,o)
     circuit.x(range(3))
     circuit.mcx([0,1,2], 3)
-    circuit.draw('mpl', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/multi_cnot_simple')
+    circuit.draw('latex', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/multi_cnot_simple.png')
     circuit.measure_all()
 
     return circuit, dir+'qcnc_paper_figures/multi_cnot_simple'
@@ -165,7 +165,7 @@ def multi_cnot_with_ancilla(dir='qcnc_paper_code/'):
     # circuit.ccx(0,1,4)
     
     # Drawing before measurement to not have the measurement features show up
-    circuit.draw('mpl', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/multi_cnot_with_ancilla')
+    circuit.draw('latex', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/multi_cnot_with_ancilla.png')
     
     circuit.measure_all()
 
@@ -188,7 +188,7 @@ def multi_cnot_with_ancilla_and_uncomp(dir='qcnc_paper_code/'):
     circuit.ccx(0,1,4)
     
     # Drawing before measurement to not have the measurement features show up
-    circuit.draw('mpl', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/multi_cnot_with_ancilla_and_uncomp')
+    circuit.draw('latex', initial_state=True, plot_barriers=False, idle_wires=False, filename=dir+'qcnc_paper_figures/multi_cnot_with_ancilla_and_uncomp.png')
     
     circuit.measure_all()
 
