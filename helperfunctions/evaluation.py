2d97023caa15eb43ef2b331491faa3d638c49b79
Ashutosh Tiwari <ashutiw2k@gmail.com>
2024-11-19 22:16:10 -0500

    Evalualtion Helper Func and Reconfig of evals

---
+++ helperfunctions/evaluation.py
@@ -0,0 +1,134 @@
+import numpy
+from qiskit import QuantumCircuit
+
+from helperfunctions.measurecircuit import get_probability_from_statevector, get_statevector, zero_ancillas_in_statevector
+
+class NumAncillaUncomped:
+    def __init__(self) -> None:
+        self.exhaustive = []
+        self.num_exhaustive = []
+        self.greedy_full = []
+        self.num_greedy_full = []
+        self.greedy_partial = []
+        self.num_greedy_partial = []
+        self.regular = []
+        self.num_regular = []
+    
+    def add_exhaustive(self, ancillas):
+        self.exhaustive.append(ancillas)
+        self.num_exhaustive.append(len(ancillas))
+
+    def add_greedy_full(self, ancillas):
+        self.greedy_full.append(ancillas)
+        self.num_greedy_full.append(len(ancillas))
+
+    def add_greedy_partial(self, ancillas):
+        self.greedy_partial.append(ancillas)
+        self.num_greedy_partial.append(len(ancillas))
+
+    def add_regular(self, ancillas):
+        self.regular.append(ancillas)
+        self.num_regular.append(len(ancillas))
+
+    def get_all_nums(self):
+        return self.num_exhaustive, self.num_greedy_full, self.num_greedy_partial
+        
+class ProbDiffResults:
+    def __init__(self, valid_num_circuits):
+        self.exhaustive_comp_diff = numpy.zeros(valid_num_circuits)
+        self.exhaustive_uncomp_diff = numpy.zeros(valid_num_circuits)
+        self.exhaustive_eq4 = numpy.zeros(valid_num_circuits)
+        self.exhaustive_eq5 = numpy.zeros(valid_num_circuits)
+        self.exhaustive_uncomp = numpy.zeros(valid_num_circuits)
+        
+
+        self.greedy_full_comp_diff = numpy.zeros(valid_num_circuits)
+        self.greedy_full_uncomp_diff = numpy.zeros(valid_num_circuits)
+        self.greedy_full_eq4 = numpy.zeros(valid_num_circuits)
+        self.greedy_full_eq5 = numpy.zeros(valid_num_circuits)
+        self.greedy_full_uncomp = numpy.zeros(valid_num_circuits)
+        
+        self.greedy_partial_comp_diff = numpy.zeros(valid_num_circuits)
+        self.greedy_partial_uncomp_diff = numpy.zeros(valid_num_circuits)        
+        self.greedy_partial_eq4 = numpy.zeros(valid_num_circuits)
+        self.greedy_partial_eq5 = numpy.zeros(valid_num_circuits)
+        self.greedy_partial_uncomp = numpy.zeros(valid_num_circuits)
+
+        self.regular_comp_diff = numpy.zeros(valid_num_circuits)
+        self.regular_uncomp_diff = numpy.zeros(valid_num_circuits)
+        self.regular_eq4 = numpy.zeros(valid_num_circuits)
+        self.regular_eq5 = numpy.zeros(valid_num_circuits)
+        self.regular_uncomp = numpy.zeros(valid_num_circuits)
+
+    def add_to_exhaustive(self, comp_diff, uncomp_diff, eq4, eq5, uncomp, idx):
+        # numpy.append(self.exhaustive_uncomp_diff, comp)
+        # numpy.append(self.exhaustive_uncomp_diff, uncomp)
+        self.exhaustive_comp_diff[idx] = comp_diff
+        self.exhaustive_uncomp_diff[idx] = uncomp_diff
+        # self.exhaustive_eq4[idx] = eq4
+        # self.exhaustive_eq5[idx] = eq5
+        # self.exhaustive_uncomp[idx] = uncomp
+
+
+    def add_to_greedy_partial(self, comp_diff, uncomp_diff, eq4, eq5, uncomp, idx):
+        # numpy.append(self.exhaustive_uncomp_diff, comp)
+        # numpy.append(self.exhaustive_uncomp_diff, uncomp)
+        self.greedy_partial_comp_diff[idx] = comp_diff
+        self.greedy_partial_uncomp_diff[idx] = uncomp_diff
+        # self.greedy_partial_eq4[idx] = eq4
+        # self.greedy_partial_eq5[idx] = eq5
+        # self.greedy_partial_uncomp[idx] = uncomp
+    
+    def add_to_greedy_full(self, comp_diff, uncomp_diff, eq4, eq5, uncomp, idx):
+        # numpy.append(self.exhaustive_uncomp_diff, comp)
+        # numpy.append(self.exhaustive_uncomp_diff, uncomp)
+        self.greedy_full_comp_diff[idx] = comp_diff
+        self.greedy_full_uncomp_diff[idx] = uncomp_diff
+        # self.greedy_full_eq4[idx] = eq4
+        # self.greedy_full_eq5[idx] = eq5
+        # self.greedy_full_uncomp[idx] = uncomp
+    
+    def add_to_regular(self, comp_diff, uncomp_diff, eq4, eq5, uncomp, idx):
+        # numpy.append(self.exhaustive_uncomp_diff, comp)
+        # numpy.append(self.exhaustive_uncomp_diff, uncomp)
+        self.regular_comp_diff[idx] = comp_diff
+        self.regular_uncomp_diff[idx] = uncomp_diff
+        # self.regular_eq4[idx] = eq4
+        # self.regular_eq5[idx] = eq5
+        # self.regular_uncomp[idx] = uncomp
+
+    def __str__(self):
+        return f'''
+                Exhaustive Comp Avg:\t\t{numpy.average(self.exhaustive_comp_diff)}
+                Exhaustive UnComp Avg:\t\t{numpy.average(self.exhaustive_uncomp_diff)}
+                Greedy Full Comp Avg:\t\t{numpy.average(self.greedy_full_comp_diff)}
+                Greedy Full UnComp Avg:\t\t{numpy.average(self.greedy_full_uncomp_diff)}
+                Greedy Partial Comp Avg:\t{numpy.average(self.greedy_partial_comp_diff)}
+                Greedy Partial UnComp Avg:\t{numpy.average(self.greedy_partial_uncomp_diff)}
+                Regular Comp Avg:\t\t{numpy.average(self.regular_comp_diff)}
+                Regular UnComp Avg:\t\t{numpy.average(self.regular_uncomp_diff)}
+                '''
+
+        
+def get_eval_results(comp_circuit: QuantumCircuit, uncomp_circuit:QuantumCircuit, num_a):
+    eq4_comp_statevector = get_statevector(comp_circuit)
+    eq4_comp_prob_dist = get_probability_from_statevector(eq4_comp_statevector)
+    # logger.info(f'Comp Circuit {name_str} Eq4 Probability Distribution: \n{print_probs(eq4_comp_prob_dist)}')
+
+    eq5_comp_statevector = zero_ancillas_in_statevector(eq4_comp_statevector, num_a)
+    eq5_comp_prob_dist = get_probability_from_statevector(eq5_comp_statevector)
+    # logger.info(f'Comp Circuit {name_str} Eq5 Probability Distribution: \n{print_probs(eq5_comp_prob_dist)}')
+
+    eq4_uncomp_statevector = get_statevector(uncomp_circuit)
+    eq4_uncomp_prob_dist = get_probability_from_statevector(eq4_uncomp_statevector)
+    # logger.info(f'{uncomp_type.capitalize()} Uncomp Circuit {name_str} Eq4 Probability Distribution: \n{print_probs(eq4_uncomp_prob_dist)}')
+
+    distance_probs_eq5_4_comp = numpy.linalg.norm(eq5_comp_prob_dist - eq4_comp_prob_dist)
+    distance_probs_eq5_4_uncomp = numpy.linalg.norm(eq4_uncomp_prob_dist - eq5_comp_prob_dist)
+    
+    distance_probs_eq5_4_comp, distance_probs_eq5_4_uncomp = numpy.round((distance_probs_eq5_4_comp, distance_probs_eq5_4_uncomp), decimals=10)
+
+    return distance_probs_eq5_4_comp, distance_probs_eq5_4_uncomp, eq4_comp_prob_dist, eq5_comp_prob_dist, eq4_uncomp_prob_dist
+
+
+
